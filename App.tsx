6928afce7cd1a0b8e6dda6cac2306d54ac391a9f
DrStoop <berr@gmx.de>
2024-06-27 20:49:49 +0300

    Simple App BEFORE BUG FIX which does not responde to the props "tabBarLabel" and "tabBarLabelStyle"

--- App.tsx
+++ App.tsx
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+function TabBarLabel(props: { focused: boolean, color: string, labelText: string }): React.ReactNode {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Text style={{ color: props.focused ? "green" : "red" }}>
+      {props.labelText}
+    </Text>
+  )
+}
+
+function HomeScreen(): React.ReactNode {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function SettingsScreen(): React.ReactNode {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createMaterialTopTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator style={{ paddingTop: 50 }}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          // The custom tab bar label is ignored and the navigation name "Home" is used nevertheless
+          tabBarLabel: "Custom Home",
+          // The custom tab bar label style is ignored and the default style is used nevertheless
+          tabBarLabelStyle: { fontSize: 20 }
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          // The custom component is ignored and the default <Text> component is used nevertheless
+          tabBarLabel: TabBarLabel
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
